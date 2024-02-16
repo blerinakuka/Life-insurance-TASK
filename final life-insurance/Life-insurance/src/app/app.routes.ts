11661280af60ebcfd71b88857fc7267753a3a08d
Blerina <blerina.kukaj@sunnyhilltech.ch>
2024-02-16 15:52:16 +0100

    swiss-home

--- final life-insurance/Life-insurance/src/app/app.routes.ts
+++ final life-insurance/Life-insurance/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { FormsComponent } from './forms/forms.component';
 import { DankeComponent } from './dankePage/danke.component';
 import { HomeComponent } from './home/home.component';
+import { SwissHomeComponent } from './swiss-home/swiss-home.component';
 
 export const routes: Routes = [
   { path: '', component: FormsComponent },
   { path: 'danke', component: DankeComponent },
-  { path: 'home', component: HomeComponent}
+  { path: 'home', component: HomeComponent},
+  {path: 'swiss-home', component: SwissHomeComponent}
 ];
 
 
---
+++ final life-insurance/Life-insurance/src/app/swiss-home/swiss-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-swiss-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './swiss-home.component.html',
+  styleUrl: './swiss-home.component.scss'
+})
+export class SwissHomeComponent {
+
+}
