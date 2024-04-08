ba48f12538cadc66cdbad28691c82e77585018a1
Blerina <blerina.kukaj@sunnyhilltech.ch>
2024-04-08 10:54:38 +0200

    deleting shf files

--- final life-insurance/Life-insurance/src/app/app.routes.ts
+++ final life-insurance/Life-insurance/src/app/app.routes.ts
@@ -2,13 +2,12 @@ import { Routes } from '@angular/router';
 import { FormsComponent } from './forms/forms.component';
 import { DankeComponent } from './dankePage/danke.component';
 import { HomeComponent } from './home/home.component';
-import { SwissHomeComponent } from './swiss-home/swiss-home.component';
+
 
 export const routes: Routes = [
   { path: '', component: FormsComponent },
   { path: 'danke', component: DankeComponent },
   { path: 'home', component: HomeComponent},
-  {path: 'swiss-home', component: SwissHomeComponent}
 ];
 
 
